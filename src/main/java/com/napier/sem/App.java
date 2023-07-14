1705639da2f14efc0d75abe7dc60d5b5a3d214c0
HtetAung-San <40614789@live.napier.ac.uk>
2023-07-14 10:14:56 +0630

    Added comments on App.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,23 +2,32 @@ package com.napier.sem;
 
 import java.sql.*;
 
+/*
+Purpose: Main App to call classes to make reports
+ */
 public class App
 {
     public static void main(String[] args)
     {
+        // Initialize the variables.
         String district = "Bíobío";
         String country = "China";
         String region = "Caribbean";
         String continent="North America";
 
+        /*
+        Establishing the sql connection for the first time
+        SQLConnection Class object is created.
+        connect() is used to connect to the database
+         */
         SQLConnection connection = new SQLConnection();
         connection.connect();
         Connection con = connection.getCon();
 
+        // Creating the classes to make reports
         AllCitiesInWorld.printResult(AllCitiesInWorld.getAllCities(con));
         AllCitiesInCountry.printResult(AllCitiesInCountry.ReturnCity(country,con));
         AllCitiesInDistrict.printResult(AllCitiesInDistrict.ReturnCity(district,con));
-
         AllCitiesInRegion.printResult(AllCitiesInRegion.ReturnCity(region,con));
         AllCitiesInContinent.printResult(AllCitiesInContinent.ReturnCity(continent,con));
 
@@ -26,6 +35,7 @@ public class App
         AllCountriesInContinent.printResult(AllCountriesInContinent.ReturnCountries(continent,con));
         AllCountriesInRegion.printResult(AllCountriesInRegion.ReturnCountries(region,con));
 
+        // disconnecting the database
         connection.disconnect();
     }
 
