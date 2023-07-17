eebcbc5ea87dea1c848805483eb2b66db2e9088f
Kyi Sin <40614786@live.napier.ac.uk>
2023-07-17 14:30:01 +0630

    all files merged

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -25,20 +25,27 @@ public class App
         Connection con = connection.getCon();
 
         // Creating the classes to make reports
+        // REPORT: All Countries in the World/Continent/Region
         AllCountriesInWorld.printResult(AllCountriesInWorld.ReturnCountries(con));
         AllCountriesInContinent.printResult(continent, AllCountriesInContinent.ReturnCountries(continent,con));
         AllCountriesInRegion.printResult(region, AllCountriesInRegion.ReturnCountries(region,con));
+
+        // REPORT: Top 10 Countries in the World/Continent/Region
         Top10CountriesInWorld.printResult(Top10CountriesInWorld.ReturnCountries(con));
         Top10CountriesInContinent.printResult(continent, Top10CountriesInContinent.ReturnCountries(continent,con));
         Top10CountriesInRegion.printResult(region, Top10CountriesInRegion.ReturnCountries(region,con));
 
-
+        // REPORT: All Cities in the World/Continent/Region/Country/District
         AllCitiesInWorld.printResult(AllCitiesInWorld.getAllCities(con));
         AllCitiesInContinent.printResult(continent, AllCitiesInContinent.ReturnCity(continent,con));
         AllCitiesInRegion.printResult(region, AllCitiesInRegion.ReturnCity(region,con));
         AllCitiesInCountry.printResult(country, AllCitiesInCountry.ReturnCity(country,con));
         AllCitiesInDistrict.printResult(district, AllCitiesInDistrict.ReturnCity(district,con));
 
+        // REPORT: Top 10 Cities in the World/Continent/Region/Country/District
+        Top10CitiesInWorld.printResult(Top10CitiesInWorld.ReturnCity(con));
+        Top10CitiesInContinent.printResult(continent, Top10CitiesInContinent.ReturnCity(continent,con));
+        Top10CitiesInRegion.printResult(region, Top10CitiesInRegion.ReturnCity(region,con));
 
 
 
