8bbb2b2ea05f672ef66f321e48ce2b47eab6bb0e
Kyi Sin <40614786@live.napier.ac.uk>
2023-07-18 15:25:23 +0630

    demo 3

--- src/main/java/com/napier/sem/AllCitiesInDistrict.java
+++ src/main/java/com/napier/sem/AllCitiesInDistrict.java
@@ -80,13 +80,14 @@ public class AllCitiesInDistrict {
 
         // For all the objects in cities arraylist, formatting and printing the values (Strings and Digits)
         for (City city :cities){
-            int n= cities.size();
-            for ( int i = 1; i<=n; i++) {
+
+            for (int  i = 1; i<=cities.size(); i++) {
                 // Printing the city object's attributes with Getter.
-                System.out.printf("| %-35s | %-40s | %-35s | %,20d | %n", i, city.getCityName(), city.getCountryName(), city.getDistrictName(), city.getCityPopulation());
-            }
+                System.out.println(i);
+                System.out.printf("| %-35s | %-40s | %-35s | %,20d | %n", city.getCityName(), city.getCountryName(), city.getDistrictName(), city.getCityPopulation());
+
         }
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------");
-
+}
     }
 }
