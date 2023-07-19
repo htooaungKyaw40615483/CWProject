4a5c404174f2f609daa6bbce5077ef86ad8769c2
HtetAung-San <40614789@live.napier.ac.uk>
2023-07-19 12:19:26 +0630

    Renamed to AllCitiesInDistrictTest.java from AppTest.java.

--- src/test/java/com/napier/sem/AllCitiesInDistrictTest.java
+++ src/test/java/com/napier/sem/AllCitiesInDistrictTest.java
@@ -2,15 +2,11 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 
-import java.sql.Connection;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-public class AppTest
+public class AllCitiesInDistrictTest
 {
     static AllCitiesInDistrict ACID;
 
