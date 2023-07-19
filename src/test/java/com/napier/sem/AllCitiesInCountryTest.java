37842557e2d7d0ff3881e460f909f94486239477
HtetAung-San <40614789@live.napier.ac.uk>
2023-07-19 14:23:29 +0630

    Imported mockito and connection. Added testing for only null country name in returnCity method

--- src/test/java/com/napier/sem/AllCitiesInCountryTest.java
+++ src/test/java/com/napier/sem/AllCitiesInCountryTest.java
@@ -1,10 +1,14 @@
 package com.napier.sem;
 
+import org.mockito.Mockito.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import java.sql.Connection;
 import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
 
 public class AllCitiesInCountryTest
 {
@@ -17,7 +21,7 @@ public class AllCitiesInCountryTest
 
     // Testing printResults
     @Test
-    void printResultsTestNull()
+    void printResultTestNull()
     {
         // will throw java.lang.NullPointerException if the null is not checked in AllCitiesInDistrict.
         ACIC.printResult(null, null);
@@ -46,7 +50,18 @@ public class AllCitiesInCountryTest
     }
 
     @Test
-    void ReturnCityTestNull(){
+    void returnCityTestNull(){
         ACIC.returnCity(null,null);
+    }
+
+    @Test
+    void returnCityConTestNull(){
+        ACIC.returnCity("China", null);
+    }
+
+    @Test
+    void returnCityCountryTestNull(){
+        Connection con = mock(Connection.class);
+        ACIC.returnCity(null, con);
     }
 }
