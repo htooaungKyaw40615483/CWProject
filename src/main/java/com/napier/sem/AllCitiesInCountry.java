92d6524572f5f2216fcb198e2f3f22e474374fe3
Kyi Sin <40614786@live.napier.ac.uk>
2023-07-20 15:01:24 +0630

    number format added in AllCitiesInCountry

--- src/main/java/com/napier/sem/AllCitiesInCountry.java
+++ src/main/java/com/napier/sem/AllCitiesInCountry.java
@@ -70,14 +70,15 @@ public class AllCitiesInCountry {
         System.out.println("-------------------------------------------All Cities in A Country by Largest Population to Smallest-------------------------------------------");
         System.out.println("| Country: " + countryName + "                                                                                         Total Cities: " + cities.size());
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.printf("| %-35s | %-40s | %-35s | %-20s | %n", "Name", "Country", "District", "Population");
+        System.out.printf("| %-37s | %-35s | %-35s | %-21s | %n", "Name", "Country", "District", "Population");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------");
+
+        int  i = 1;
 
         // For all the objects in cities arraylist, formatting and printing the values (Strings and Digits)
         for (City city :cities){
-
             // Printing the city object's attributes with Getter.
-            System.out.printf("| %-35s | %-40s | %-35s | %,20d | %n", city.getCityName(), city.getCountryName(), city.getDistrictName(), city.getCityPopulation());
+            System.out.printf("| %,3d | %-31s | %-35s | %-34s  | %,20d  |  %n", i++,  city.getCityName(), city.getCountryName(), city.getDistrictName(), city.getCityPopulation());
         }
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------");
 
