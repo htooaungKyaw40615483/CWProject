7865398c960f9baf430d2754ea066c2bb7de4c8c
Kyi Sin <40614786@live.napier.ac.uk>
2023-07-21 15:44:08 +0630

    demo modified

--- src/main/java/com/napier/sem/Top10CountriesInWorld.java
+++ src/main/java/com/napier/sem/Top10CountriesInWorld.java
@@ -60,7 +60,7 @@ public class Top10CountriesInWorld {
     public static void printResult(ArrayList<Country> countries){
         System.out.println("---------------------------------------------------Top 10 Countries in the World By Largest Population To Smallest------------------------------");
         System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.printf("| %,2d | %-4s | %-25s | %-22s | %-30s | %-20s | %-33s | %n", "Code", "Name", "Continent", "Region", "Population", "Capital");
+        System.out.printf(" | %-7s | %-25s | %-22s | %-30s | %-20s | %-33s | %n", "Code", "Name", "Continent", "Region", "Population", "Capital");
         System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
 
         int  i = 1;
