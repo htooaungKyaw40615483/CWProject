bb14fb9d38a24d11beaf770104d5ac236c210508
LordMercy555 <40614794@live.napier.ac.uk>
2023-07-21 15:07:31 +0630

    add unit test files for Top10CountriesInRegion

--- src/main/java/com/napier/sem/Top10CountriesInRegion.java
+++ src/main/java/com/napier/sem/Top10CountriesInRegion.java
@@ -13,6 +13,9 @@ public class Top10CountriesInRegion {
      * @return the Country Objects in an ArrayList which is from a single region.
      */
     public static ArrayList<Country> returnCountries(String regionName, Connection con){
+        if (regionName == null){
+            System.out.println("The Region name is not defined.");
+        }
         try{
             // Creating Statement Object to execute Query
             Statement stmt = con.createStatement();
@@ -61,6 +64,23 @@ public class Top10CountriesInRegion {
      */
 
     public static void printResult(String regionName, ArrayList<Country> countries){
+        if(regionName == null && countries == null){
+            System.out.println("There is no country or defined region name");
+            return;
+        }
+
+        // Check if countries arraylist is null. If not, move on to the next condition.
+        if (countries == null) {
+            System.out.println("There is no countries");
+            return;
+        }
+
+        // Check if region name is null. If not, move on to the next condition.
+        if(regionName == null){
+            System.out.println("The district name is not defined");
+            return;
+        }
+
         System.out.println("---------------------------------------------------Top 10 Countries in the Region By Largest Population To Smallest----------------------------------------------------------------");
         System.out.println("| Region: " + regionName + "                                                                                                                         ORDER: Largest to Smallest Population|");
         System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
