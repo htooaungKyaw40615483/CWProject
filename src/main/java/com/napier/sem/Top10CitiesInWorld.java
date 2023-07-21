912414e7882d722ed459f30fe700ce8322dccc86
Kyi Sin <40614786@live.napier.ac.uk>
2023-07-21 12:28:17 +0630

    number format added for Top10CitiesInWorld

--- src/main/java/com/napier/sem/Top10CitiesInWorld.java
+++ src/main/java/com/napier/sem/Top10CitiesInWorld.java
@@ -67,14 +67,14 @@ public class Top10CitiesInWorld {
     public static void printResult(ArrayList<City> cities){
         System.out.println("------------------------------------------------------Top 10 Populated Cities in The World-----------------------------------------------------");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.printf("| %-35s | %-40s | %-35s | %-20s | %n", "Name", "Country", "District", "Population");
+        System.out.printf("| %-37s | %-38s | %-35s | %-20s | %n", "Name", "Country", "District", "Population");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------");
 
         // For all the objects in cities arraylist, formatting and printing the values (Strings and Digits)
         for (City city :cities){
 
             // Printing the city object's attributes with Getter.
-            System.out.printf("| %-35s | %-40s | %-35s | %,20d | %n", city.getCityName(), city.getCountryName(), city.getDistrictName(), city.getCityPopulation());
+            System.out.printf("| %,2d | %-35s | %-40s | %-35s | %,20d | %n", city.getCityName(), city.getCountryName(), city.getDistrictName(), city.getCityPopulation());
         }
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------");
 
