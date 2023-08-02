dd7840f387d141b5483b07b7eb78035e2c14056d
Kyi Sin <40614786@live.napier.ac.uk>
2023-08-02 16:23:37 +0630

    added unit test file for CityPopulation

---
+++ src/test/java/com/napier/sem/CityPopulationTest.java
@@ -0,0 +1,80 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+public class CityPopulationTest {
+    static CityPopulation CP;
+
+    @BeforeAll
+    static void init(){
+        CP = new CityPopulation();
+    }
+
+    // Testing printResults
+    @Test
+    void printResultTestNull()
+    {
+        // will throw java.lang.NullPointerException if the null is not checked in CityPopulation.
+        CP.printResult(null, null);
+    }
+
+    @Test
+    void printResultCitiesTestNull(){
+        CP.printResult("Blackpool", null);
+    }
+
+    @Test
+    void printResultDnTestNull(){
+        ArrayList<Population> populations = new ArrayList<>();
+        Population p = new Population();
+        populations.add(p);
+        CP.printResult(null, populations);
+    }
+
+    @Test
+    void printResultCityTestNull(){
+        ArrayList<Population> populations = new ArrayList<Population>();
+        Population population = new Population();
+        population.setTotalPopulation(99999999);
+        populations.add(population);
+        CP.printResult("Blackpool", populations);
+    }
+
+    @Test
+    void returnCityTestNull(){
+        CP.returnPopulation(null,null);
+    }
+    @Test
+    public void statementQueryTesting() throws SQLException {
+        // Creating the mock connection, statement, and result set.
+        Connection mockCon = mock(Connection.class);
+        Statement mockStatement = mock(Statement.class);
+        ResultSet mockResultSet = mock(ResultSet.class);
+
+        // Return mock statement when the createStatement() is called.
+        when(mockCon.createStatement()).thenReturn(mockStatement);
+
+        // Return mock result set when the executeQuery() is called.
+        when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
+
+        Statement stmt = mockCon.createStatement();
+
+        // assert a statement that is not null
+        assertNotNull(stmt);
+
+        // creating the mock statement with the mock connection, with the
+        verify(mockCon, times(1)).createStatement();
+    }
+}
