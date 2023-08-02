d95f842ea89be1fe784eb414743e73bfcb09ae20
Kyi Sin <40614786@live.napier.ac.uk>
2023-08-02 15:46:15 +0630

    added unit test file for WorldPopulation

---
+++ src/test/java/com/napier/sem/WorldPopulationTest.java
@@ -0,0 +1,56 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+public class WorldPopulationTest {
+    static WorldPopulation WP;
+    @BeforeAll
+    static void init(){
+        WP = new WorldPopulation();
+    }
+
+    // Testing printResults
+    @Test
+    void printResultTestNull()
+    {
+        // will throw java.lang.NullPointerException if the null is not checked in WorldPopulation.
+        WP.printResult(null);
+    }
+
+    @Test
+    void returnPopulationTestNull(){
+        WP.returnPopulation(null);
+    }
+    @Test
+    public void statementQueryTesting() throws SQLException {
+        // Creating the mock connection, statement, and result set.
+        Connection mockCon = mock(Connection.class);
+        Statement mockStatement = mock(Statement.class);
+        ResultSet mockResultSet = mock(ResultSet.class);
+
+        // Return mock statement when the createStatement() is called.
+        when(mockCon.createStatement()).thenReturn(mockStatement);
+
+        // Return mock result set when the executeQuery() is called.
+        when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
+
+        Statement stmt = mockCon.createStatement();
+
+        // assert a statement that is not null
+        assertNotNull(stmt);
+
+        // creating the mock statement with the mock connection, with the
+        verify(mockCon, times(1)).createStatement();
+    }
+}
